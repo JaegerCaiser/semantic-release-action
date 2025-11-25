d35d574656f23dca384bce87337d88b8942ed5c0
Matheus Henrique Caiser Barrozo <matheus.kaiser@gmail.com>
2025-11-25 01:29:14 -0300

    fix: correctly handle dependency installation and errors

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const exec = require('./src/_exec');
 const path = require('path');
-const core = require('@actions/core');
 
 const run = async () => {
   // Install Dependencies
   try {
-    const {stdout} = await exec('npm install --only=prod --no-audit --silent', {
+    const { stdout } = await exec('npm install --only=prod --no-audit --silent', {
       cwd: path.resolve(__dirname)
     });
     console.log(stdout);
   } catch (error) {
-    core.setFailed(`Failed to install dependencies: ${error.message}`);
-    return;
+    console.error(`Failed to install dependencies: ${error.message}`);
+    process.exit(1);
   }
 
+  // Now that dependencies are installed, we can require the main logic.
   require('./src/index')();
 };
 
